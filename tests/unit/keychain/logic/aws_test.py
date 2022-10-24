6597485b743a579d5745bbeb15b9eab63c7c8041
Erick G. Islas-Osuna <erickisos653@gmail.com>
2022-10-23 21:04:28 -0500

    Add json functions (#37)

--- tests/unit/keychain/logic/aws_test.py
+++ tests/unit/keychain/logic/aws_test.py
@@ -1,9 +1,21 @@
-from keychain.logic.aws import lambda_json
+from pytest import raises
+
+from keychain.logic.aws import from_json, to_json
 
 
-def test_lambda_json():
-    assert {'statusCode': 404, 'body': '{}'} == lambda_json(404, {})
-    assert {'statusCode': 200, 'body': '[]'} == lambda_json(200, [])
-    assert {'statusCode': 201, 'body': '"hello world"'} == lambda_json(
-        201, 'hello world'
+def test_to_json():
+    assert {'statusCode': 404, 'body': '{}'} == to_json({}, 404)
+    assert {'statusCode': 200, 'body': '[]'} == to_json([], 200)
+    assert {'statusCode': 201, 'body': '"hello world"'} == to_json(
+        'hello world', 201
     )
+
+
+def test_from_json():
+    with raises(KeyError):
+        from_json({})
+
+    assert {} == from_json({'body': '{}'})
+    assert [] == from_json({'body': '[]'})
+    assert 'hello world' == from_json({'body': '"hello world"'})
+    assert {'a': 1} == from_json({'body': '{"a": 1}'})
