ab6226a164e92ea1e10c64f6e8e41987bde1494e
Erick G. Islas-Osuna <erickisos653@gmail.com>
2022-10-26 10:55:42 -0500

    Add component map (#47)

--- handlers/service.py
+++ handlers/service.py
@@ -1,6 +1,14 @@
+import os
+
+from keychain.components.telegram import Bot
 from keychain.contracts.outputs.aws import LambdaApiResponse
+from keychain.models.components import Components
 from keychain.ports.http_inputs import webhook
+
+components: Components = {
+    'telegram-bot': Bot(os.environ['TELEGRAM_BOT_TOKEN'])
+}
 
 
 def webhook_handler(event, _) -> LambdaApiResponse:
-    return webhook(event)
+    return webhook(event, components)
