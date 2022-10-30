0bbdf6abda9be8523882d37c039f2e8bad9b356c
Erick G. Islas-Osuna <erickisos653@gmail.com>
2022-10-29 23:58:30 -0500

    fix: mypy issue

--- src/keychain/controllers/telegram.py
+++ src/keychain/controllers/telegram.py
@@ -1,13 +1,10 @@
-from typing import Optional
-
 from ..models.telegram import Message, Update
 
 
-def send_message(update: Update, components) -> Optional[Message]:
+def send_message(update: Update, components) -> Message:
     message = update.edited_message or update.message
 
     if not message:
-        return
+        raise ValueError('Message not found')
 
-    # TODO: Add send_message business logic
     return components['bot'].send_message(message)
